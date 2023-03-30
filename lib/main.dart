60f2f85ae44cb0c37661bcfc281836bdc1e69163
Ale <alejandrolopezaguilar.dev@gmail.com>
2023-03-30 14:49:43 +0200

    🎨 style(main.dart): cambiar el título de la aplicación a "App convertidor"

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter Demo'),
+          title: Text('App convertidor'),
         ),
         body: Center(
           child: Padding(
